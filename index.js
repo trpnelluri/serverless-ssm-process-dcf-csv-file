21a71f2fd12562b18e122e654dc3ba35a8e8172d
SivakoteswaraRaoNell <sivakoteswararao.nelluri@cms.hhs.gov>
2022-04-28 18:53:56 -0400

    Updated the code to build the flatfile record

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//const PostgresPromiseService = require('./sharedLib/db/bkup-postgres-promise-service');
-//const ProcessEventService = require('./lib/bkup-process-event-service');
 const PostgresPoolService = require('./sharedLib/db/postgre-pool-service');
 const ProcessEventService = require('./lib/process-event-service');
 
@@ -11,8 +9,6 @@ module.exports.handler = async function (event, context, callback) {
 
   let processEventService = ProcessEventService.getInstance();
   try {
-    //const db = await PostgresPromiseService.getInstance().connectToPostgresPromiseDB ()
-    //let response = await processEventService.processEvent(event, db);
     const pool = await PostgresPoolService.getInstance().connectToPostgresDB ()
     let response = await processEventService.processEvent(event, pool);
     console.log(`handler,response: ${JSON.stringify(response)}`);
